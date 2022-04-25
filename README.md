5248ee420c36a0d797b5b8316d02a03c5c8f5482
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2022-04-25 15:37:17 +0300

    Add validation methods

---
+++ README.md
@@ -0,0 +1,28 @@
+## RUBY VALIDATIONS
+
+> A ruby application to check the validations.
+
+## Running the code
+
+- To run the code, make sure ruby is installed in your machine
+- the run `ruby validation.rb`
+
+## IMPORTANT INFORMATION!!
+
+> I have tried to best to work on this but i could figure out the exact right way to do it on time, considering today is the submission deadline i was given.
+I know i should have done better, and please if it is possible share the solution to this problem.
+
+> With this project now i know i lack alot in ruby, and it taught me not to rely on the rails 'magic', i started reading a ruby book to solidify my fundamental ruby skills.
+
+## Built With
+
+
+- Ruby 
+
+## Author
+
+👤 **Isaac Lumato**
+
+- GitHub: [@Isaac](https://github.com/isaka-lumato)
+
+- LinkedIn: [Isaac](https://www.linkedin.com/in/isaka-william-90773020b/)
--- validation.rb
+++ validation.rb
@@ -1,35 +1,32 @@
 module Validation
-
-def validate(name, presence:true)
-  if name == nil || name == ""
-    newEr =  "has not to be empty"
-    puts newEr
-  else 
-    newEr = ""
+  def validate(attrib, options)
+    if (options = presence (true))
+      if (attrib == nil || attrib == "")
+       return "please enter correct imput"
+      end
+    elsif (options = format (x))
+      if attrib !~ x
+        return ("please enter correct imput")
+      end
+    end
   end
-  return newEr
-end
 
-def valid?
-  if newEr == ""
-    return true
-  else
-    return false
+  def valid?
+    if validate(attrib, options).include? "please"
+      return false
+    else
+      return true
+    end
+  end
 end
-
-end
-end
-
 
 class User
   include Validation
-
   attr_accessor :name
-  validate :name, presence:true
   def initialize(name)
     @name = name
   end
-
 end
-c = User.new("") 
-puts c.valid?
+
+c = User.new(nil)
+c.validate(:name, presence:true)
